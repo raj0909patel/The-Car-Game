5135c2d8ed1d5ee6e7dd52ad8e848467d740b0c5
Raj Patel <raj0909patel@gmail.com>
2015-09-07 21:45:38 +0530

    My first change
    Uploading the files

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMovement : MonoBehaviour {
+
+	public int speed;
+	public int rotate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveY = Input.GetAxis ("Vertical");
+
+		transform.Translate(new Vector3 (0.0f, 0.0f, -moveY * speed));
+		transform.Rotate(new Vector3 (0.0f, 1f, 0.0f), moveX * rotate);
+		//transform.rotation = transform.rotation + Quaternion.AngleAxis(moveX * speed, new Vector3(0.0f, 1f , 0.0f));
+		//transform.position = transform.position + (new Vector3 (0.0f, 0.0f, moveY));
+		//transform.localPosition = transform.position + (new Vector3 (0.0f, 0.0f, moveY));
+
+	}
+}
