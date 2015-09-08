78abd9f3e9d452ab05d7233c8be1ab2118730eed
Raj Patel <raj0909patel@gmail.com>
2015-09-08 17:05:43 +0530

    Changes 01
    Camera attached to car
    Rotation Limited
    Car moves automatically at constant speed

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public GameObject car;
+	
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		offset = transform.position - car.transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	
+		transform.position = car.transform.position + offset;
+	}
+}
--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class CarMovement : MonoBehaviour {
 
-	public int speed;
-	public int rotate;
+	public float speed;
+	public float rotate;
+
+	private bool start = false;
+	//private bool keydown = false;
 
 	// Use this for initialization
 	void Start () {
@@ -14,14 +17,17 @@ public class CarMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		float moveX = Input.GetAxis ("Horizontal");
-		float moveY = Input.GetAxis ("Vertical");
 
-		transform.Translate(new Vector3 (0.0f, 0.0f, -moveY * speed));
-		transform.Rotate(new Vector3 (0.0f, 1f, 0.0f), moveX * rotate);
-		//transform.rotation = transform.rotation + Quaternion.AngleAxis(moveX * speed, new Vector3(0.0f, 1f , 0.0f));
-		//transform.position = transform.position + (new Vector3 (0.0f, 0.0f, moveY));
-		//transform.localPosition = transform.position + (new Vector3 (0.0f, 0.0f, moveY));
 
-	}
+		if (Input.GetButtonDown("Fire1"))
+			start = true;
+		if (start) {
+			transform.Translate (new Vector3 (0.0f, 0.0f, -0.25f));
+		}
+
+			float moveX = Input.GetAxis ("Horizontal") * rotate;
+			transform.Rotate(new Vector3 (0.0f, 1f, 0.0f),moveX);
+
+		transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,Mathf.Clamp(transform.localEulerAngles.y,145,215), transform.localEulerAngles.z);
+		}
 }
